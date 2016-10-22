1273fc9f5cce0f7441b0bc41fbd9d239a2d207eb
Shamal Perera <uslperera@gmail.com>
2016-10-22 06:52:04 +0530

    Added the extraction part of docx

---
+++ src/zipExtractor.js
@@ -0,0 +1,71 @@
+var JSZip = require("jszip");
+
+exports.openArrayBuffer = openArrayBuffer;
+
+function openArrayBuffer(arrayBuffer) {
+    var zipFile = new JSZip(arrayBuffer);
+    function exists(name) {
+        return zipFile.file(name) !== null;
+    }
+    
+    function read(name, encoding) {
+        var array = zipFile.file(name).asUint8Array();
+        var buffer = new Buffer(array);
+        if (encoding) {
+            return promises.when(buffer.toString(encoding));
+        } else {
+            return promises.when(buffer);
+        }
+    }
+    
+    function write(name, contents) {
+        zipFile.file(name, contents);
+    }
+    
+    function toBuffer() {
+        return zipFile.generate({type: "nodebuffer"});
+    }
+    
+    return {
+        exists: exists,
+        read: read,
+        write: write,
+        toBuffer: toBuffer
+    };
+}
+
+var fs = require('fs');
+var Promise = require('promise');
+var unzip = require('unzip');
+
+function openFile(path){
+    return new Promise(function(resolve, reject){
+        fs.readFile(path, 'utf8', function(err, data){
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        });
+    });
+}
+
+fs.createReadStream('/Users/shamal/Documents/Projects/Hello Docx.docx').pipe(unzip.Extract({ path: '/Users/shamal/Documents/Projects/' }));
+/*
+openFile("/Users/shamal/Documents/Projects/Hello Docx.docx.zip").then(function(data){
+    //console.log(data);
+    // xml2js.parseString(data, function(err, result){
+    //  console.dir(result);
+
+    // });
+    var open = openArrayBuffer(data);
+    open.write("aa", data)
+    // XMLExtract(data, 'w:body', false, (error, element) => {
+    //         if(error){
+    //             throw new Error(error);
+    //         }
+    //         console.log(element);
+
+    //     });
+});
+*/
